9fc919aab288c2a99a50a59efeb5de26bf4d3da1
Tanisha Jain <tanishajn12@gmail.com>
2024-08-13 17:36:08 +0530

    Day 01

---
+++ template.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] : %(message)s:')
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "."
+]
